c4fb99d5d12a95aab59e64b87c06bb2615a2d420
Minewarpro <hugo.roche13@hotmail.com>
2022-02-07 15:53:09 +0100

    Update Tableau1.js

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -1,54 +1,54 @@
 /**
  * ALGO: ceci est une classe...
  */
-class Tableau1 extends Phaser.Scene{
+class Tableau1 extends Phaser.Scene {
     /**
      * Précharge les assets
      */
-    preload(){
+    preload() {
         this.load.image('carre', 'assets/carre.png');
         this.load.image('cercle', 'assets/cercle.png');
 
     }
 
 
-    create(){
+    create() {
         let me = this
 
         this.largeur = 800
 
         /** Création de raquette **/
 
-        this.raquette = this.physics.add.sprite(this.largeur/2, this.largeur-20, 'carre');
-        this.raquette.setDisplaySize(200,20);
+        this.raquette = this.physics.add.sprite(this.largeur / 2, this.largeur - 20, 'carre');
+        this.raquette.setDisplaySize(200, 20);
         this.raquette.body.setAllowGravity(false);
         this.raquette.setImmovable(true);
 
 
         /** Création de balle **/
 
-        this.balle = this.physics.add.sprite(this.largeur/2, 500, 'cercle');
-        this.balle.setDisplaySize(20,20);
-        this.balle.body.setBounce(1.3,1.3);
-        this.balle.body.setMaxVelocityX(400,400)
-        this.balle.body.setMaxVelocityY(600,600)
+        this.balle = this.physics.add.sprite(this.largeur / 2, 500, 'cercle');
+        this.balle.setDisplaySize(20, 20);
+        this.balle.body.setBounce(1.3, 1.3);
+        this.balle.body.setMaxVelocityX(400, 400)
+        this.balle.body.setMaxVelocityY(600, 600)
 
         this.balle.setVelocityY(-200);
 
         /** Création de Mur **/
 
-        this.murHaut = this.physics.add.sprite(0, 0, 'carre').setOrigin(0,0);
-        this.murHaut.setDisplaySize(800,20);
+        this.murHaut = this.physics.add.sprite(0, 0, 'carre').setOrigin(0, 0);
+        this.murHaut.setDisplaySize(800, 20);
         this.murHaut.body.setAllowGravity(false);
         this.murHaut.setImmovable(true);
 
-        this.murGauche = this.physics.add.sprite(0, 0, 'carre').setOrigin(0,0);
-        this.murGauche.setDisplaySize(20,800);
+        this.murGauche = this.physics.add.sprite(0, 0, 'carre').setOrigin(0, 0);
+        this.murGauche.setDisplaySize(20, 800);
         this.murGauche.body.setAllowGravity(false);
         this.murGauche.setImmovable(true);
 
-        this.murDroit = this.physics.add.sprite(this.largeur-20, 0, 'carre').setOrigin(0,0);
-        this.murDroit.setDisplaySize(20,800);
+        this.murDroit = this.physics.add.sprite(this.largeur - 20, 0, 'carre').setOrigin(0, 0);
+        this.murDroit.setDisplaySize(20, 800);
         this.murDroit.body.setAllowGravity(false);
         this.murDroit.setImmovable(true);
 
@@ -91,7 +91,7 @@ class Tableau1 extends Phaser.Scene{
 
                 this.physics.add.collider(this.balle, rect, function () {
                     me.rebond(me.obstacles[i]);
-                    me.disparait(me.obstacles[i+m*10]);
+                    me.disparait(me.obstacles[i + m * 10]);
                     me.Joueur.score++;
                 });
             }
@@ -101,86 +101,84 @@ class Tableau1 extends Phaser.Scene{
     }
 
 
-    rebond(raquette){
+    rebond(raquette) {
 
-        let me=this;
+        let me = this;
 
 
         let hauteurRaquette = raquette.displayHeight;
 
-        let positionRelativeRaquette =(this.balle.x-raquette.x);
+        let positionRelativeRaquette = (this.balle.x - raquette.x);
 
-        positionRelativeRaquette = (positionRelativeRaquette/hauteurRaquette);
+        positionRelativeRaquette = (positionRelativeRaquette / hauteurRaquette);
 
-        positionRelativeRaquette = (positionRelativeRaquette*2-1);
+        positionRelativeRaquette = (positionRelativeRaquette * 2 - 1);
         console.log(positionRelativeRaquette);
 
-        this.balle.setVelocityX( this.balle.body.velocity.x + positionRelativeRaquette * hauteurRaquette)
+        this.balle.setVelocityX(this.balle.body.velocity.x + positionRelativeRaquette * hauteurRaquette)
 
     }
 
-    disparait(obstacle){
-        let me=this;
+    disparait(obstacle) {
+        let me = this;
         obstacle.disableBody(true);
         obstacle.setVisible(false);
     }
 
-    Initiale (){
+    Initiale() {
         let me = this
+        this.red = 1
+        this.green = 1
 
-        this.balle.setX(this.largeur/2);
-        this.balle.setY(this.largeur-50);
+        this.balle.setX(this.largeur / 2);
+        this.balle.setY(this.largeur - 50);
         this.balle.setVelocityY(-200);
         this.balle.setVelocityX(0);
-        this.raquette.setX(this.largeur/2);
+        this.raquette.setX(this.largeur / 2);
 
         this.obstacles[10].setTintFill(0xFF0000FF);
 
         me.Joueur.vie = 3;
         me.Joueur.score = 0;
 
-        for(let i=0;i<me.obstacles.length;i++){
+        for (let i = 0; i < me.obstacles.length; i++) {
             me.obstacles[i].setVisible(true);
             me.obstacles[i].body.setEnable(true);
         }
 
     }
 
-    loose(){
-        this.balle.setX(this.largeur/2);
-        this.balle.setY(this.largeur-50);
+    loose() {
+        this.balle.setX(this.largeur / 2);
+        this.balle.setY(this.largeur - 50);
         this.balle.setVelocityY(-200);
         this.balle.setVelocityX(0);
-        this.raquette.setX(this.largeur/2);
+        this.raquette.setX(this.largeur / 2);
 
         this.Joueur.vie--;
 
-        if (this.Joueur.vie == 0){
+        if (this.Joueur.vie == 0) {
             alert('Perdu');
             this.Initiale();
         }
     }
 
     initKeyboard() {
-        let me=this;
-        this.input.keyboard.on('keydown', function(kevent)
-        {
-            switch (kevent.keyCode)
-            {
+        let me = this;
+        this.input.keyboard.on('keydown', function (kevent) {
+            switch (kevent.keyCode) {
                 case Phaser.Input.Keyboard.KeyCodes.RIGHT:
-                    if (me.raquette.x < me.largeur-120) {
+                    if (me.raquette.x < me.largeur - 120) {
                         me.raquette.setVelocityX(300)
-                    }
-                    else {
-                        me.raquette.setX(me.largeur-100)
+                    } else {
+                        me.raquette.setX(me.largeur - 100)
                         me.raquette.setVelocityX(0)
                     }
                     break;
                 case Phaser.Input.Keyboard.KeyCodes.LEFT:
                     if (me.raquette.x > 120) {
                         me.raquette.setVelocityX(-300)
-                    }
-                    else {
+                    } else {
                         me.raquette.setX(100)
                         me.raquette.setVelocityX(0)
                     }
@@ -190,10 +188,8 @@ class Tableau1 extends Phaser.Scene{
             }
 
         });
-        this.input.keyboard.on('keyup', function(kevent)
-        {
-            switch (kevent.keyCode)
-            {
+        this.input.keyboard.on('keyup', function (kevent) {
+            switch (kevent.keyCode) {
                 case Phaser.Input.Keyboard.KeyCodes.RIGHT:
                     me.raquette.setVelocityX(0)
                     break;
@@ -205,24 +201,16 @@ class Tableau1 extends Phaser.Scene{
         });
     }
 
-    update(){
-        if(this.balle.y > this.largeur){
+    update() {
+        if (this.balle.y > this.largeur) {
             this.loose();
         }
 
-        if(this.balle.x < 0){
+        if (this.balle.x < 0) {
             this.balle.x = 0
         }
-        if(this.balle.x > this.largeur){
+        if (this.balle.x > this.largeur) {
             this.balle.x = this.largeur
         }
-        if (red == 1){
-            if (me.obstacles[35].visible = true){
-
             }
-        }
-
-    }
-
-
 }
